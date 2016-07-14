c915a045fff49905ed5d05f90c8d274e7d153ff1
Jan-Oliver Kaiser <janno@mpi-sws.org>
2016-07-14 16:04:40 +0200

    replace '-' in devname with '_'

--- collectd_lxc.py
+++ collectd_lxc.py
@@ -116,6 +116,7 @@ def reader(input_data=None):
 		        devname = None
 		        with open('/sys/dev/block/%s/uevent' % k, 'r') as f:
 			    devname = re_dev.search(f.read()).group('devname')
+			    devname = devname.replace('-', '_')
 
                         values = collectd.Values(plugin_instance=lxc_fullname,
                                                  type="%s" % devname, plugin="lxc_blkio")
